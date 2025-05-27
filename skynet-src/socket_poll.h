1890efb55efcce1eb55a1125bc2139d2ef9518ba
cxldada <c.x.l@live.com>
2025-05-27 20:42:13 +0800

    解决冲突

--- skynet-src/socket_poll.h
+++ skynet-src/socket_poll.h
@@ -6,28 +6,28 @@
 typedef int poll_fd;
 
 struct event {
-	void * s;
-	bool read;
-	bool write;
-	bool error;
-	bool eof;
+    void* s;
+    bool read;
+    bool write;
+    bool error;
+    bool eof;
 };
 
 static bool sp_invalid(poll_fd fd);
 static poll_fd sp_create();
 static void sp_release(poll_fd fd);
-static int sp_add(poll_fd fd, int sock, void *ud);
+static int sp_add(poll_fd fd, int sock, void* ud);
 static void sp_del(poll_fd fd, int sock);
-static int sp_enable(poll_fd, int sock, void *ud, bool read_enable, bool write_enable);
-static int sp_wait(poll_fd, struct event *e, int max);
+static int sp_enable(poll_fd, int sock, void* ud, bool read_enable, bool write_enable);
+static int sp_wait(poll_fd, struct event* e, int max);
 static void sp_nonblocking(int sock);
 
 #ifdef __linux__
-#include "socket_epoll.h"
+#    include "socket_epoll.h"
 #endif
 
-#if defined(__APPLE__) || defined(__FreeBSD__) || defined(__OpenBSD__) || defined (__NetBSD__)
-#include "socket_kqueue.h"
+#if defined(__APPLE__) || defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__)
+#    include "socket_kqueue.h"
 #endif
 
 #endif
